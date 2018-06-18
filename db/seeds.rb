9fe9d319d9f694000944eee32390b6020d8684bd
github username <github email address>
2018-06-18 17:44:34 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,8 @@ c1 = Comment.create(content: "c1, blah blah blah", task_id: 1)
 c2 = Comment.create(content: "c2, blah blah blah", task_id: 3)
 c3 = Comment.create(content: "c3, blah blah blah", task_id: 4)
 
+s1 = Status.create(status: "to do", task_id: 1)
+s2 = Status.create(status: "to do", task_id: 3)
+s3 = Status.create(status: "doing", task_id: 2)
+s4 = Status.create(status: "to do", task_id: 5)
+s5 = Status.create(status: "done", task_id: 4)
